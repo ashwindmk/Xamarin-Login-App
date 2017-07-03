b66059ae7cb9a88c137f49db28c3ea93bd451a75
Ashwin <ashwin.dinesh01@gmail.com>
2017-07-03 07:04:58 +0530

    Created Xamarin Login App project

---
+++ Xamarin-Login-App/Xamarin_Login_App/HomePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Xamarin_Login_App
+{
+	public partial class HomePage : ContentPage
+	{
+		public HomePage (String username)
+		{
+			InitializeComponent ();
+
+            WelcomeLabel.Text = "Welcome " + username;
+        }
+	}
+}
---
+++ Xamarin-Login-App/Xamarin_Login_App/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_Login_App
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void LoginButtonClicked(Object sender, EventArgs e)
+        {
+            UserDataLabel.Text = "Please login";
+
+            string username = UsernameEntry.Text;
+            string password = PasswordEntry.Text;
+
+            if ( string.Equals(username, "ashwin", StringComparison.CurrentCultureIgnoreCase) && string.Equals(password, "pass123")
+                || string.Equals(username, "bob", StringComparison.CurrentCultureIgnoreCase) && string.Equals(password, "qwerty")
+                || string.Equals(username, "carrie", StringComparison.CurrentCultureIgnoreCase) && string.Equals(password, "password")
+            )
+            {
+                UserDataLabel.Text = "Last logged in as " + username;
+
+                // Go to home page
+                await Navigation.PushAsync(new HomePage(username));
+            }
+            else
+            {
+                UserDataLabel.Text = "Invalid username or password";
+            }
+            
+        }
+	}
+}
